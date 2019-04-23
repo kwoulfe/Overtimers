54241d59a574df7aa4dbf4b3dcd28d68ba83e6b7
Kevin Woulfe <kevinpwoulfe@gmail.com>
2019-04-22 23:32:56 -0700

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+First draft of a portfolio website for a client.
+
+https://joshlippi.herokuapp.com/
+
+More to come...
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+
+var api_key = 'b2779cf26713e4af14b4ae95db085e07-3fb021d1-89e691f9';
+var domain = 'mail.joshlippi.com';
+var mailgun = require('mailgun-js')({ apiKey: api_key, domain: domain });
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
+router.post('/contact', function(req, res) {
+  var data = {
+    from: req.body.email,
+    to: 'kevinpwoulfe@gmail.com',
+    subject: 'New Message from ' + req.body.name,
+    text: req.body.message
+  };
+
+  console.log(data);
+
+  mailgun.messages().send(data, function(error, body) {
+    if (error) {
+      console.log(error);
+    }
+    console.log(body);
+  });
+});
+
+module.exports = router;
